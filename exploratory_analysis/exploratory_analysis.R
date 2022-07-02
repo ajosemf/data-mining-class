1381e3e61aa5222a6d07a39c2b3e9fd4051e7534
ajosemf <ajosemf@gmail.com>
2022-07-02 18:59:20 -0300

    Add plot and remove canceled flights

---
+++ exploratory_analysis/exploratory_analysis.R
@@ -0,0 +1,29 @@
+source("data-mining-class/utils/myBasic.R")
+source("data-mining-class/utils/myGraphic.R")
+
+
+colors <- brewer.pal(11, 'Paired')
+font <- theme(text = element_text(size=16))
+loadlibrary("MASS")
+
+load("data-mining-class/data/bfd.rda")
+
+colnames(bfd)
+
+# plot and removing canceled flights
+bfd_cancel = bfd %>% dplyr::filter(situation_type == "CANCELADO")
+unique(bfd_cancel %>% dplyr::select(arrival_delay))
+delay_cancel = bfd_cancel %>% dplyr::count(arrival_delay)
+delay_cancel
+delay_not_zero_cancel = delay_cancel %>% dplyr::filter(arrival_delay != 0)
+delay_not_zero_cancel
+sum(delay_not_zero_cancel$n)
+barplot(delay_not_zero_cancel$n,
+        main = "Arrival delays on canceled flights",
+        xlab = "Arrival delay value",
+        ylab = "# Arrival delay",
+        names.arg = delay_not_zero_cancel$arrival_delay,
+        col = "darkred")
+bfd = bfd %>% dplyr::filter(situation_type != "CANCELADO")
+
+
